bd54b42c7fdb620e3ed6f4e2f24b81555e5217b5
Vivek Das Mohapatra <vivek@collabora.com>
2025-12-22 20:40:20 +0000

    renaming: update comments, copyright headers, etc to use 'holo-efi'

--- chainloader/util.c
+++ chainloader/util.c
@@ -1,21 +1,21 @@
-// steamos-efi  --  SteamOS EFI Chainloader
+// holo-efi  --  Holo EFI Chainloader
 
 // SPDX-License-Identifier: GPL-2.0+
 // Copyright © 2018-2024 Collabora Ltd
 // Copyright © 2018-2024 Valve Corporation
 
-// steamos-efi is free software: you can redistribute it and/or modify
+// holo-efi is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2.0 of the License, or
 // (at your option) any later version.
 
-// steamos-efi is distributed in the hope that it will be useful,
+// holo-efi is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with steamos-efi.  If not, see <http://www.gnu.org/licenses/>.
+// along with holo-efi.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <efi.h>
 #include <efilib.h>
